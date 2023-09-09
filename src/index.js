d68d58ee229b34c4a5ebd49e91bc9d8d3f59d953
Camera Kendall <camerakendall@gmail.com>
2023-09-09 15:49:03 -0400

    started basic gravity effect

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ console.log('hello world')
 const game = document.querySelector('canvas')
 
 const g = game.getContext("2d")
-g.fillStyle = 'red'
+g.fillStyle = 'yellow'
 g.fillRect(10, 10, 100, 100)
 
 
@@ -12,36 +12,49 @@ let { canvas, context } = init(game);
 console.log(canvas)
 console.log(context)
 
-canvas.height = innerHeight
-canvas.width = innerWidth
+canvas.height = 576
+canvas.width = 1024
 
 context.fillStyle = '#156464'
 context.fillRect(0, 0, canvas.width, canvas.height)
 
-let sprite = Sprite({
-  x: 100,        // starting x,y position of the sprite
-  y: 80,
-  color: 'blue',  // fill color of the sprite rectangle
+
+const gravity = 0.5
+
+const player = Sprite({
+  x: 200,        // starting x,y position of the sprite
+  y: 200,
+  color: 'yellow',  // fill color of the sprite rectangle
   width: 20,     // width and height of the sprite rectangle
   height: 40,
-  dx: 2          // move the sprite 2px to the right every frame
+  dx: 2,          // move the sprite 2px to the right every frame
+  dy: 2
 });
 
-let loop = GameLoop({  // create the main game loop
+const loop = GameLoop({  // create the main game loop
   update: function() { // update the game state
-    sprite.update();
+    player.update();
 context.fillStyle = '#156464'
 context.fillRect(0, 0, canvas.width, canvas.height)
     // wrap the sprites position when it reaches
     // the edge of the screen
-    if (sprite.x > canvas.width) {
-      sprite.x = -sprite.width;
+    if (player.x > canvas.width) {
+      player.x = -player.width;
     }
+    //beginning gravity logic
+    if(player.y + player.height + player.dy < canvas.height) {
+      // console.log('i\'m in the air')
+      player.dy += gravity
+    } else {
+      player.dy = 0
+    }
+
+
   },
   render: function() { // render the game state
     context.fillStyle = '#156464'
     context.fillRect(0, 0, canvas.width, canvas.height)
-    sprite.render();
+    player.render();
   }
 });
 
