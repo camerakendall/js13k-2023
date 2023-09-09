648513ceb4c2b0c722de71d4ee3ec37a07778149
Camera Kendall <camera@macbook-pro.mynetworksettings.com>
2023-09-09 12:55:33 -0400

    background color

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import { init, Sprite, GameLoop } from 'kontra';
+import { getCanvas, getContext, init, Sprite, GameLoop } from 'kontra';
 
 console.log('hello world')
 const game = document.querySelector('canvas')
 
-let { canvas } = init(game);
-console.log(game)
+const g = game.getContext("2d")
+g.fillStyle = 'yellow'
+g.fillRect(10, 10, 100, 100)
 
+
+let { canvas, context } = init(game);
+console.log(canvas)
+console.log(context)
+
+canvas.height = innerHeight
+canvas.width = innerWidth
+
+context.fillStyle = '#156464'
+context.fillRect(0, 0, canvas.width, canvas.height)
 
 let sprite = Sprite({
   x: 100,        // starting x,y position of the sprite
@@ -19,7 +30,8 @@ let sprite = Sprite({
 let loop = GameLoop({  // create the main game loop
   update: function() { // update the game state
     sprite.update();
-
+context.fillStyle = '#156464'
+context.fillRect(0, 0, canvas.width, canvas.height)
     // wrap the sprites position when it reaches
     // the edge of the screen
     if (sprite.x > canvas.width) {
@@ -27,6 +39,8 @@ let loop = GameLoop({  // create the main game loop
     }
   },
   render: function() { // render the game state
+    context.fillStyle = '#156464'
+    context.fillRect(0, 0, canvas.width, canvas.height)
     sprite.render();
   }
 });
