c0be0803c44905e624d8bed38b42ef9702091c75
Camera Kendall <camerakendall@gmail.com>
2023-09-09 18:09:16 -0400

    added player image asset

--- src/index.js
+++ src/index.js
@@ -1,66 +1,18 @@
 import { getCanvas, getContext, init, Sprite, GameLoop } from 'kontra';
 
-console.log('hello world')
 const game = document.querySelector('canvas')
 
-const g = game.getContext("2d")
-g.fillStyle = 'yellow'
-g.fillRect(10, 10, 100, 100)
+// const g = game.getContext("2d")
+// g.fillStyle = 'yellow'
+// g.fillRect(10, 10, 100, 100)
 
 
 let { canvas, context } = init(game);
-console.log(canvas)
-console.log(context)
+// console.log(canvas)
+// console.log(context)
 
 canvas.height = 576
 canvas.width = 1024
-
-context.fillStyle = '#156464'
-context.fillRect(0, 0, canvas.width, canvas.height)
-
-
-const gravity = 0.5
-
-const player = Sprite({
-  x: 200,        // starting x,y position of the sprite
-  y: 200,
-  color: 'yellow',  // fill color of the sprite rectangle
-  width: 20,     // width and height of the sprite rectangle
-  height: 40,
-  dx: 2,          // move the sprite 2px to the right every frame
-  dy: 2
-});
-
-const loop = GameLoop({  // create the main game loop
-  update: function() { // update the game state
-    player.update();
-    context.fillStyle = '#156464'
-    context.fillRect(0, 0, canvas.width, canvas.height)
-    // wrap the sprites position when it reaches
-    // the edge of the screend
-    player.dx = 0
-    if (keys.d.pressed) player.dx = 10
-    else if (keys.a.pressed) player.dx = -10
-
-    if (player.x > canvas.width) {
-      player.x = -player.width;
-    }
-    //beginning gravity logic
-    if(player.y + player.height + player.dy < canvas.height) {
-      // console.log('i\'m in the air')
-      player.dy += gravity
-    } else {
-      player.dy = 0
-    }
-
-
-  },
-  render: function() { // render the game state
-    context.fillStyle = '#156464'
-    context.fillRect(0, 0, canvas.width, canvas.height)
-    player.render();
-  }
-});
 
 const keys = {
   w: {
@@ -102,5 +54,62 @@ window.addEventListener('keyup', (event) => {
   }
 })
 
+
+const gravity = 0.5
+const image = new Image()
+image.src = 'img/knight.png'
+console.log(image.src)
+console.log(image)
+
+const player = Sprite({
+  x: 200,        // starting x,y position of the sprite
+  y: 200,
+  // color: 'yellow',  // fill color of the sprite rectangle
+  // width: image.width,     // width and height of the sprite rectangle
+  // height: image.height,
+  dx: 2,          // move the sprite 2px to the right every frame
+  dy: 2,
+  image: image
+
+});
+
+image.onload= () => {
+  player.width = image.width
+  player.height = image.height + player.dy
+  player.render()
+}
+
+
+const loop = GameLoop({  // create the main game loop
+  update: function() { // update the game state
+    player.update();
+    context.fillStyle = '#156464'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+    // wrap the sprites position when it reaches
+    // the edge of the screend
+    player.dx = 0
+    if (keys.d.pressed) player.dx = 10
+    else if (keys.a.pressed) player.dx = -10
+
+    if (player.x > canvas.width) {
+      player.x = -player.width;
+    }
+    //beginning gravity logic
+    if(player.y + player.height + player.dy < canvas.height) {
+      // console.log('i\'m in the air')
+      player.dy += gravity
+    } else {
+      player.dy = 0
+    }
+
+
+  },
+  render: function() { // render the game state
+    context.fillStyle = '#052318'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+    player.render();
+    
+  }
+});
 
 loop.start();    // start the game
